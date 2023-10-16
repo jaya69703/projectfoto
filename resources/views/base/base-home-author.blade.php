80fd07aab1807719e15e3e2bf39fbc19e3b6dcce
jaya69703 <jaya69703@gmail.com>
2023-10-16 19:29:39 +0700

    Susun Ulang Author Dashboard

--- resources/views/base/base-home-author.blade.php
+++ resources/views/base/base-home-author.blade.php
@@ -5,12 +5,12 @@
             <div class="card btn btn-outline-primary">
                 <div class="card-body d-flex justify-content-around align-items-center">
                     <span class="icon"><i class="fa-solid fa-edit" style="font-size: 50px"></i></span>
-                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Proses Editing Foto <br> {{ App\Models\Booking::where('book_stat', '6')->count() }}</span>
+                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Proses Editing <br> {{ App\Models\Booking::where('book_stat', '6')->count() }}</span>
                 </div>
             </div>
         </a>
     </div>
-    <div class="col-lg-3 col-12 mb-2">
+    {{-- <div class="col-lg-3 col-12 mb-2">
         <a href="{{ route('author.booking.view') }}">
             <div class="card btn btn-outline-success">
                 <div class="card-body d-flex justify-content-around align-items-center">
@@ -19,13 +19,13 @@
                 </div>
             </div>
         </a>
-    </div>
+    </div> --}}
     <div class="col-lg-3 col-12 mb-2">
         <a href="{{ route('admin.booking.pending') }}">
             <div class="card btn btn-outline-secondary">
                 <div class="card-body d-flex justify-content-around align-items-center">
                     <span class="icon"><i class="fa-solid fa-newspaper" style="font-size: 50px"></i></span>
-                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Artikel Publish <br> {{ App\Models\Booking::where('book_stat', '0')->count() }} X</span>
+                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Artikel Publish <br> {{ App\Models\Post::all()->count() }} </span>
                 </div>
             </div>
         </a>
@@ -34,8 +34,18 @@
         <a href="{{ route('admin.booking.pending') }}">
             <div class="card btn btn-outline-warning">
                 <div class="card-body d-flex justify-content-around align-items-center">
-                    <span class="icon"><i class="fa-solid fa-newspaper" style="font-size: 50px"></i></span>
-                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Jumlah Artikel <br> {{ App\Models\Booking::where('book_stat', '0')->count() }} X</span>
+                    <span class="icon"><i class="fa-solid fa-folder" style="font-size: 50px"></i></span>
+                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Jumlah Kategori <br> {{ App\Models\CategoryB::all()->count() }}</span>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-lg-3 col-12 mb-2">
+        <a href="{{ route('admin.booking.pending') }}">
+            <div class="card btn btn-outline-success">
+                <div class="card-body d-flex justify-content-around align-items-center">
+                    <span class="icon"><i class="fa-solid fa-tags" style="font-size: 50px"></i></span>
+                    <span class="text-white" style="margin-left: 20px; font-size: 16px;">Jumlah Tags <br> {{ App\Models\Tagsb::all()->count() }} </span>
                 </div>
             </div>
         </a>
